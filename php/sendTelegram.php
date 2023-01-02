f2ff72a96f94cf2f74e9f48c9c2f1a11686a1f8d
Zaharov Viktor <viktorz2000@gmail.com>
2023-01-02 12:39:04 +0200

    hv

---
+++ php/sendTelegram.php
@@ -0,0 +1,36 @@
+ <?php
+
+$quantity = $_POST['quantity'];
+$price = $_POST['price'];
+$token = "5784483416:AAEjn-wB_DFSWGP2LV54Uj-W43ocMlqN6wI";
+$chat_id = "-1001812409420";
+
+
+/* ОТПРАВКА ПИСЬМА ЗАКАЗА В TELEGRAM */
+/*функция для создания запроса на сервер Telegram */
+$arr = array(
+  "Новая завка с  сайта";
+  'Количество (кг):' => $quantity;
+  'Стоимость (грн):' => $price;
+);
+
+foreach($arr as $key => $value) {
+  $txt .= '<b>'.$key.'</b> '.$value.'%0A';
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text={$txt}", 'd');
+
+
+// $message .= "Новая завка с  сайта";
+// $message .= "Количество (кг): ".$quantity;
+// $message .= "Стоимость: ".$price;
+
+// function orderSendTelegram($message) {
+
+
+
+//     parser("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text={$message}");
+
+// }
+
+?> 
